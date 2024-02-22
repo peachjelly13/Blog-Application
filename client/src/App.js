81c9c93e2c2f28ae928d8c5c76b9b5f0d71f5582
peachjelly13 <ananya.sharma2021a@vitstudent.ac.in>
2024-02-22 15:59:42 +0530

    full stack blog application

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MY Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href = "">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <img src="https://media.istockphoto.com/id/1352603244/photo/shot-of-an-unrecognizable-businessman-working-on-his-laptop-in-the-office.jpg?s=1024x1024&w=is&k=20&c=mTABddPRSU1r_hCBpknMjJbCIrJAicjjXGSU42rx-YI="></img>
+         <div class="texts">
+         <h2>Write form anywhere</h2>
+         <p>Remote jobs have changed the way people work it has completely changed the worl wide view and has made it easier for people to work from anywhere and anyhow</p>
+         </div>
+      </div>
+      <div className="post">
+      <img src="https://media.istockphoto.com/id/1352603244/photo/shot-of-an-unrecognizable-businessman-working-on-his-laptop-in-the-office.jpg?s=1024x1024&w=is&k=20&c=mTABddPRSU1r_hCBpknMjJbCIrJAicjjXGSU42rx-YI="></img>
+         <div class="texts">
+         <h2>Write form anywhere</h2>
+         <p>Remote jobs have changed the way people work it has completely changed the worl wide view and has made it easier for people to work from anywhere and anyhow</p>
+         </div>
+         
+      </div>
+      <div className="post">
+        <img src="https://media.istockphoto.com/id/1352603244/photo/shot-of-an-unrecognizable-businessman-working-on-his-laptop-in-the-office.jpg?s=1024x1024&w=is&k=20&c=mTABddPRSU1r_hCBpknMjJbCIrJAicjjXGSU42rx-YI="></img>
+         <div class="texts">
+         <h2>Write form anywhere</h2>
+         <p>Remote jobs have changed the way people work it has completely changed the worl wide view and has made it easier for people to work from anywhere and anyhow</p>
+         </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
