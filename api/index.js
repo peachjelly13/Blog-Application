183cf780b2ce3f7ca77ebbb12774e59a71119006
peachjelly13 <ananya.sharma2021a@vitstudent.ac.in>
2024-03-02 20:14:30 +0530

    backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+require('dotenv').config();
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const {User} = require('./models/User');
 const app = express();
 app.use(cors());
-app.post('/register',(req,res)=>{
-    res.json('ok')
-})
+app.use(express.json());
+const URI = process.env.MONGO_URI;
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+
+app.post('/register',async (req,res)=>{
+    const {username,password} = req.body;
+    await User.create({username,password});
+    res.json({requestData:{username,password}});
+});
 
 app.listen(4000);
+
